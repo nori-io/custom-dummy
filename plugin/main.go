2e3a1e4b68caea7680fb104a0d65d212a5354243
bruteforce1414 <bruteforce1414@gmail.com>
2021-05-13 10:53:16 +0500

    feat: plugin was build, change package plugin->main

--- plugin/main.go
+++ plugin/main.go
@@ -11,7 +11,7 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
-package plugin
+package main
 
 import (
 	"context"
